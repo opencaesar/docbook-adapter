034ac96cd8a1dbd8f0e1844a8905e73e8e2e3eef
truongdavid98 <truongdavid98@gmail.com>
2020-07-07 14:41:10 -0700

    More setup
    Updated to include the necessary dependencies

--- docbook-tools/docbook-adapter/src/main/java/io/opencaesar/docbook/adapter/App.java
+++ docbook-tools/docbook-adapter/src/main/java/io/opencaesar/docbook/adapter/App.java
@@ -1,9 +1,108 @@
 package io.opencaesar.docbook.adapter;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.common.io.CharStreams;
 
 public class App {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+	/*
+	@Parameter(
+		names = { "--endpoint", "-e" },
+		description = "Sparql Endpoint URL.  (Required)",
+		required = true,
+		order = 1)
+	String endpoint;
+		
+	@Parameter(
+		names = { "--query", "-q" },
+		description = "Path to the .sparql query file (Required)",
+		required = true,
+		order = 3)
+	String queriesPath;
+	
+	@Parameter(
+		names = { "--result", "-r" },
+		description = "Path to the folder to save the result to (Required)",
+		required = true,
+		order = 4)
+	String resultPath;
+	
+	@Parameter(
+		names = { "--format", "-f" },
+		description = "Format of the results. Must be either xml, json, csv, n3, ttl, n-triple or tsv (Required)",
+		validateWith = FormatType.class, 
+		required = false,
+		order = 4)
+	String formatType = "xml";
+
+	@Parameter(
+		names = { "-d", "--debug" },
+		description = "Shows debug logging statements",
+		order = 9)
+	private boolean debug;
+
+	@Parameter(
+		names = { "--help", "-h" },
+		description = "Displays summary of options",
+		help = true,
+		order =10)
+	private boolean help;
+	
+	private final Logger LOGGER = LogManager.getLogger("Owl Query"); {
+		LOGGER.setLevel(Level.INFO);
+		PatternLayout layout = new PatternLayout("%r [%t] %-5p %c %x - %m%n");
+		LOGGER.addAppender(new ConsoleAppender(layout));
+	}
+
+	public static void main(final String... args) {
+		final App app = new App();
+		final JCommander builder = JCommander.newBuilder().addObject(app).build();
+		builder.parse(args);
+		if (app.help) {
+			builder.usage();
+			return;
+		}
+		if (app.debug) {
+			final Appender appender = LogManager.getRootLogger().getAppender("stdout");
+			((AppenderSkeleton) appender).setThreshold(Level.DEBUG);
+		}
+	    try {
+			app.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() throws Exception {
+		LOGGER.info("=================================================================");
+		LOGGER.info("                        S T A R T");
+		LOGGER.info("                     OWL Query ");
+		LOGGER.info("=================================================================");
+		//App logic
+	    LOGGER.info("=================================================================");
+		LOGGER.info("                          E N D");
+		LOGGER.info("=================================================================");
+		}
+	*/
 }
