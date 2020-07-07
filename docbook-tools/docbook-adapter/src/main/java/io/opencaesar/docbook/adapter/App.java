9c82523d34e8a250730668ac93c8c9e46a93341b
truongdavid98 <truongdavid98@gmail.com>
2020-07-07 13:48:03 -0700

    Gradle setup
    Set up gradle for docbook adapter. Maven not set up yet, however.

---
+++ docbook-tools/docbook-adapter/src/main/java/io/opencaesar/docbook/adapter/App.java
@@ -0,0 +1,9 @@
+package io.opencaesar.docbook.adapter;
+
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
